a3dfdc6d557fa0a8f0ef280af05f92adf2f7adac
ryanyogan <ryanyogan@gmail.com>
2019-11-06 08:50:33 -0800

    foo

---
+++ lib/http_server.ex
@@ -0,0 +1,14 @@
+defmodule HttpServer do
+  @port 8000
+  @http_options [active: false, packet: :http_bin, reuseaddr: true]
+
+  def init() do
+    {:ok, socket} = :gen_tcp.listen(@port, @http_options)
+    accept(socket)
+  end
+
+  def accept(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    pid = spawn(HttpServer, :handle, [client])
+  end
+end
